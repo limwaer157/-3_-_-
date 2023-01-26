3bd9742bdba46d9f8f452cf922c574056300bcd1
Denis <limwaer157@gmail.com>
2023-01-26 15:01:46 +0300

    firts

---
+++ 003/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int[] array = {1, 2, 43, 23, 455, 123, 12, 23,43,};
+
+// int n = array.Length;
+// int find = 43;
+// int index = 0;
+
+// while (index < n )
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index = index + 1;
+// }
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index = index + 1;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position = position + 1;
+    } 
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+           position = index;
+        }
+        index = index + 1;
+    }
+    return  position;
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexOf(array,  4);
+Console.WriteLine(pos);
---
+++ массив array/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+ 
+//  int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if(arg2> result) result = arg2;
+//     if(arg3> result) result = arg3;
+//     return result;
+// }
+ int[] array = {11, 22, 31, 23, 123, 1221, 1221, 45, 987, 22};
+ array[0] = 12;
+ int max = Max(max);
+ Console.WriteLine(array[0]);
